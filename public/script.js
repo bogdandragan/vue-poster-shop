491e1cc57712de2756054a5c3191cc2a7ac84934
bogdan <bogdan@dragan.com.ua>
2018-11-13 12:42:00 +0200

    add products

--- public/script.js
+++ public/script.js
@@ -1,17 +1,38 @@
 var PRICE = 9.99;
+var LOAD_NUM = 10;
 
 new Vue({
     el: '#app',
     data:{
         total: 0,
-        items: [
-            { id: 1, title: 'Item 1 '},
-            { id: 2, title: 'Item 2 '},
-            { id: 3, title: 'Item 3 '}
-        ],
-        cart: []
+        items: [],
+        cart: [],
+        results: [],
+        search : "anime",
+        lastSearch : "",
+        loading: false,
+        price: PRICE
     },
     methods: {
+        appendItems: function() {
+            if(this.items.length < this.results.length){
+                var append = this.results.slice(this.items.length, this.items.length + LOAD_NUM);
+                this.items = this.items.concat(append);
+            }
+        },
+        onSubmit: function(){
+            if(this.search.length){
+                this.items = [];
+                this.loading = true;
+                this.$http.get('/search/'.concat(this.search)).then(function(res){
+                    console.log(res);
+                    this.lastSearch = this.search;
+                    this.results = res.data;
+                    this.appendItems();
+                    this.loading = false;
+                })
+            }
+        },
         addItem: function(index) {
             this.total += PRICE;
             var item = this.items[index];
@@ -55,5 +76,22 @@ new Vue({
         currency: function(price){
             return '$'.concat(price.toFixed(2));
         }
+    },
+    computed: {
+        noMoreItems: function(){
+            return this.items.length === this.results.length && this.results.length > 0
+        }
+    },
+    mounted: function() {
+        this.onSubmit();
+
+        var vieInstance = this;
+        var elem = document.getElementById('product-list-bottom');
+        var watcher = scrollMonitor.create(elem);
+        watcher.enterViewport(function() {
+            vieInstance.appendItems();
+        })
     }
+
 });
+
